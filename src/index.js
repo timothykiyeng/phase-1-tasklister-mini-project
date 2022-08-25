e935e4c6f88364f2c8575df16d23eceba08c7753
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-25 07:45:13 +0300

    Completed Challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createMyToDo(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function createMyToDo(tasks) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  li.textContent = `${tasks} `;
+  btn.textContent = "x";
+  li.appendChild(btn);
+  console.log(li);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
